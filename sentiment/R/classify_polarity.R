2a16c278eb233a824edf9b498371a9de373f4d5b
Jeffrey Wong <jeff.ct.wong@gmail.com>
2014-08-29 09:05:44 -0700

    exponentially faster classify polarity

--- sentiment/R/classify_polarity.R
+++ sentiment/R/classify_polarity.R
@@ -1,60 +1,35 @@
-classify_polarity <- function(textColumns,algorithm="bayes",pstrong=0.5,pweak=1.0,prior=1.0,verbose=FALSE,...) {
-	matrix <- create_matrix(textColumns,...)
-	lexicon <- read.csv(system.file("data/subjectivity.csv.gz",package="sentiment"),header=FALSE)
-
-	counts <- list(positive=length(which(lexicon[,3]=="positive")),negative=length(which(lexicon[,3]=="negative")),total=nrow(lexicon))
-	documents <- c()
-
-	for (i in 1:nrow(matrix)) {
-		if (verbose) print(paste("DOCUMENT",i))
-		scores <- list(positive=0,negative=0)
-		doc <- matrix[i,]
-		words <- findFreqTerms(doc,lowfreq=1)
-		
-		for (word in words) {
-			index <- pmatch(word,lexicon[,1],nomatch=0)
-			if (index > 0) {
-				entry <- lexicon[index,]
-				
-				polarity <- as.character(entry[[2]])
-				category <- as.character(entry[[3]])
-				count <- counts[[category]]
-	
-				score <- pweak
-                if (polarity == "strongsubj") score <- pstrong
-				if (algorithm=="bayes") score <- abs(log(score*prior/count))
-		
-				if (verbose) {
-                    print(paste("WORD:",word,"CAT:",category,"POL:",polarity,"SCORE:",score))
-				}
-				
-				scores[[category]] <- scores[[category]]+score
-			}		
-		}
-		
-		if (algorithm=="bayes") {
-			for (key in names(scores)) {
-				count <- counts[[key]]
-				total <- counts[["total"]]
-				score <- abs(log(count/total))
-				scores[[key]] <- scores[[key]]+score
-			}
-		} else {
-			for (key in names(scores)) {
-				scores[[key]] <- scores[[key]]+0.000001
-			}
-		}
-		
-        best_fit <- names(scores)[which.max(unlist(scores))]
-        ratio <- as.integer(abs(scores$positive/scores$negative))
-        if (ratio==1) best_fit <- "neutral"
-		documents <- rbind(documents,c(scores$positive,scores$negative,abs(scores$positive/scores$negative),best_fit))
-		if (verbose) {
-			print(paste("POS:",scores$positive,"NEG:",scores$negative,"RATIO:",abs(scores$positive/scores$negative)))
-			cat("\n")
-		}
-	}
-	
-	colnames(documents) <- c("POS","NEG","POS/NEG","BEST_FIT")
-	return(documents)
+classify_polarity = function (textColumns,
+                               pstrong = 0.5, pweak = 1, prior = 1, 
+                               verbose = FALSE, ...) 
+{
+  matrix <- create_matrix(textColumns, ...)
+  matrix.dt = data.table(doc = matrix$i,
+                         word = matrix$dimnames$Terms[matrix$j],
+                         v = matrix$v)
+  lexicon <- data.table(read.csv(system.file("data/subjectivity.csv.gz", 
+                                             package = "sentiment"),
+                                 header = FALSE,
+                                 stringsAsFactors = F))
+  setnames(lexicon, colnames(lexicon), c("word", "strength", "sentiment"))
+  counts <- lexicon[,table(sentiment)]
+  counts.dt = lexicon[,list(count = .N), list(sentiment)]
+  
+  matrix.dt = merge(matrix.dt, lexicon, all.x=T, allow.cartesian = T, by = 'word')
+  matrix.dt = merge(matrix.dt, counts.dt, all.x=T, by = 'sentiment')
+  matrix.dt[,score := (log(ifelse(strength == "strongsubj", pstrong, pweak) * prior / count))]
+  
+  bar = matrix.dt[,list(score = sum(score)),list(doc, sentiment)]
+  priors = data.table(melt((log(counts/nrow(lexicon)))))
+  
+  bar = data.table(merge.data.frame(priors, bar, all.x = T, by = NULL))[(as.character(sentiment.x) == sentiment.y) | (is.na(sentiment.y))]
+  bar = bar[,list(score = sum(score, na.rm=T),
+                  prior = mean(value)),
+            list(sentiment.x, doc)]
+  bar[,score2 := score + prior]
+  scores = data.table(dcast(bar, doc ~ sentiment.x, value.var = 'score2'))
+  
+  scores[,best_fit := ifelse(abs(positive) > abs(negative), "positive", ifelse(positive == negative, "neutral", "negative"))]
+  
+  doc.dt = data.table(doc = 1:length(textColumns))
+  return (merge(doc.dt, scores, by = 'doc', all.x=T))
 }

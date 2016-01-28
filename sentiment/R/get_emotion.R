568cb8819546b17e16df21f1e447927365979be5
Alexander Loth <home@alexloth.com>
2016-01-28 16:44:39 +0100

    Update get_emotion.R

--- sentiment/R/get_emotion.R
+++ sentiment/R/get_emotion.R
@@ -1,13 +1,5 @@
 get_emotion = function (textColumns, ...) {
   emotion_analysis <- classify_emotion(textColumns, ...)
   result <- toString(emotion_analysis[[1,7]])
-  if (result!='anger'
-      && result!='fear'
-      && result!='joy'
-      && result!='sadness'
-      && result!='surprise'
-      && result!='NA') {
-    result <- 'anticipation'
-  }
   return (result)
 }

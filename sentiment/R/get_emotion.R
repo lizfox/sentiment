7429dbd5da33948334ed692c157c77debba82cf8
lizfox <Elizabeth.Fox001@umb.edu>
2016-07-27 09:34:00 -0400

    Add files via upload

--- sentiment/R/get_emotion.R
+++ sentiment/R/get_emotion.R
@@ -1,5 +1,5 @@
 get_emotion = function (textColumns, ...) {
   emotion_analysis <- classify_emotion(textColumns, ...)
-  result <- toString(emotion_analysis[[1,8]])
+  result <- toString(emotion_analysis[[1,9]])
   return (result)
 }
